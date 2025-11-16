e3b152a21196b8affb669399033ea86dc81f4baa
mjanders6 <mjanders6@gmail.com>
2025-11-15 21:31:46 -0800

    start file

--- Minecraft/config/start.sh
+++ Minecraft/config/start.sh
@@ -2,5 +2,10 @@
 
 envsubst < /opt/minecraft/server/server.properties.template > /opt/minecraft/server/server.properties
 
+# Print the resolved file
+echo "==== FINAL server.properties ===="
+cat /opt/minecraft/server/server.properties
+echo "================================="
+
 # Start Minecraft server
 java -Xmx4096M -Xms2048M -jar server.jar nogui
